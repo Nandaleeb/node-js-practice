e258cf6ce88265e52bc7d9648bd316ab34f69947
Nandaleeb <noshiandaleeb@gmail.com>
2026-05-19 11:22:31 -0400

    secound work

---
+++ Desktop/node.js_practice/hello.js
@@ -0,0 +1,5 @@
+var sayHello = function(name) {
+    return `hello ${name}`;
+}
+
+module.exports = sayHello;
--- Desktop/node.js_practice/server.js
+++ Desktop/node.js_practice/server.js
@@ -1,16 +1,10 @@
 const http = require('http');
-function sayHello(name) {
-    return name('asif', 'hamid');
-}
-function name(firstName, lastName) {
-    return `hello ${firstName} ${lastName}`;
-}
+const hello= require('./hello.js')
 
 http.createServer((req, res) => {
 res.write('this is my first server.')
-res.write(sayHello(name));
-
+res.write (hello('Eira'))
 res.end();
-}).listen(3002, () => {
-	console.log('server is running on http://localhost:3002')
+}).listen(3004, () => {
+	console.log('server is running on http://localhost:3004')
 })
